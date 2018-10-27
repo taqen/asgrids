65ab760ed9124c88436e0b709276a444b5edb78e
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-10-28 00:32:09 +0200

    fixing pickling and send issues in async_communication

--- async_communication.py
+++ async_communication.py
@@ -4,14 +4,12 @@ import zmq
 import threading
 import asyncio
 import zmq.asyncio
-import concurrent.futures
 import logging
-import msgpack
-
-from threading import Event as ThreadEvent
+import _pickle as pickle
 
 # Get the local logger
 logger = logging.getLogger(__name__)
+
 
 class AsyncCommunication(threading.Thread):
     def __init__(self, identity=None):
@@ -20,14 +18,11 @@ class AsyncCommunication(threading.Thread):
         self._receive_callback = None
         self._log = logging.getLogger(__name__)
 
-        name = 'AsyncCommThread'
-        self.identity=identity
-        if identity:
-            name = name+identity
+        self.identity = identity
 
         self.context = None
-        self.cliSocket = None
-        self.srvSocket = None
+        self.client = None
+        self.server = None
         self.poller = None
         self.timeout = 1000
         self.retries = 10
@@ -35,9 +30,11 @@ class AsyncCommunication(threading.Thread):
         self.context = zmq.asyncio.Context()
         self.poller = zmq.asyncio.Poller()
         self.loop = asyncio.new_event_loop()
-        self.loop.set_debug(True)
+        # self.loop.set_debug(True)
 
-        self.executor = self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        name = 'AsyncCommThread'
+        if identity:
+            name = name+identity
 
         super(AsyncCommunication, self).__init__(name=name)
 
@@ -45,78 +42,85 @@ class AsyncCommunication(threading.Thread):
         asyncio.set_event_loop(self.loop)
         self.loop.run_forever()
 
-    async def _send(self, request, remote = None):
-        if self.cliSocket is None:
-            self.cliSocket = self.context.socket(zmq.DEALER)
+    async def _send(self, request, remote=None):
+        if self.client is None:
+            print("creating client")
+            self.client = self.context.socket(zmq.DEALER)
         else:
-            self.poller.unregister(self.cliSocket)
-            self.cliSocket.close()
-            self.cliSocket = self.context.socket(zmq.DEALER)
+            print("client already created")
+            self.poller.unregister(self.client)
+            self.client.close()
+            self.client = self.context.socket(zmq.DEALER)
 
         if self.identity is not None:
-            self.cliSocket.setsockopt(zmq.IDENTITY, msgpack.packb(self.identity))
+            print("identity is %s" % self.identity)
+            identity = pickle.dumps(self.identity)
+            try:
+                self.client.setsockopt(zmq.IDENTITY, identity)
+            except Exception:
+                print("Error while setting socket identity")
+
         if not remote:
             msg = 'no socket_address provided'
-            self.executor.submit(print, msg)
+            print(msg)
             raise ValueError(msg)
-        else:
-            socket_address = 'tcp://{}'.format(remote)
-            self.executor.submit(print, 'sending ' + request + ' to ' + socket_address)
 
-        self.cliSocket.connect(socket_address)
-        self.poller.register(self.cliSocket, zmq.POLLIN)
+        socket_address = 'tcp://{}'.format(remote)
 
-        self.executor.submit(print,'sending')
-        await self.cliSocket.send_multipart(msgpack.packb(request))
+        print("Connecting to {}".format(socket_address))
+        try:
+            self.client.connect(socket_address)
+            self.poller.register(self.client, zmq.POLLIN)
+        except Exception:
+            print("Error connecting client socket")
+
+        print('sending {} to {}'.format(request, socket_address))
+        await self.client.send_multipart([pickle.dumps(request)])
 
     async def _run_server(self, callback=None):
-        self.executor.submit(print, 'running server')
+        self.poller.register(self.server, zmq.POLLIN)
+        print('running server')
         while True:
             items = dict(await self.poller.poll(self.timeout))
-            if self.srvSocket in items and items[self.srvSocket] == zmq.POLLIN:
-                ident, msg = await self.srvSocket.recv_multipart()
-                msg = msgpack.unpackb(msg, encoding='utf-8')
-                ident = msgpack.unpackb(ident, encoding='utf-8')
-                self.executor.submit(print, 'server received {} from {}'.format(msg, ident))
-                self.executor.submit(callback, data=msg, src=ident)
+            if self.server in items and items[self.server] == zmq.POLLIN:
+                print("receiving at server")
+                ident, msg = await self.server.recv_multipart()
+                msg = pickle.loads(msg)
+                ident = pickle.loads(ident).decode()
+                print('server received {} from {}'.format(msg, ident))
+                callback(data=msg, src=ident)
                 #TODO respond after server receive
-                # await self.srvSocket.send_multipart(msg)
-            if self.cliSocket in items and items[self.cliSocket] == zmq.POLLIN:
-                ident, msg = await self.cliSocket.recv_multipart()
-                self.executor.submit(print, 'received ack at client socket')
+                # await self.server.send_multipart(msg)
+            if self.client in items and items[self.client] == zmq.POLLIN:
+                ident, msg = await self.client.recv_multipart()
+                print('received ack at client socket')
                 #TODO Should this be further handled?
-                msg = msgpack.unpackb(msg, encoding='utf-8')
-                ident = msgpack.unpackb(ident, encoding='utf-8')
+                msg = pickle.loads(msg)
+                ident = pickle.loads(ident)
                 self.executor.submit(callback, data=msg, src=ident)
 
-    def run_server(self,callback=None, local_address=None):
+    def run_server(self, callback=None, local_address=None):
         if not (local_address):
             msg = 'At least TCP address must be used.'
-            self.executor.submit(logger.critical, msg)
             raise ValueError(msg)
         if self.identity is None:
             self.identity = local_address.encode()
-        self.executor.submit(print,'Server listening on address tcp://{}.'.format(local_address))
+        print('Server listening on address tcp://{}.'.format(local_address))
 
-        self.srvSocket = self.context.socket(zmq.ROUTER)
-        self.srvSocket.bind('tcp://{}'.format(local_address))
-
-        self.poller.register(self.srvSocket, zmq.POLLIN)
+        self.server = self.context.socket(zmq.ROUTER)
+        self.server.bind('tcp://{}'.format(local_address))
 
         asyncio.run_coroutine_threadsafe(
             self._run_server(callback=callback), self.loop)
 
-
-    def send(self, request, remote = None):
+    def send(self, request, remote=None):
+        print("send {} to {}".format(request, remote))
         asyncio.run_coroutine_threadsafe(
             self._send(request, remote=remote), self.loop)
 
-
     def stop(self):
-        logger.debug('CommunicationAddress {!r}: stop thread.'.format(self.name))
-        self.poller.unregister(self.cliSocket)
-        self.poller.unregister(self.srvSocket)
+        self.poller.unregister(self.client)
+        self.poller.unregister(self.server)
         self.loop.stop()
         self.context.destroy()
-        exit()
 
