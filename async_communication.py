e2735e4cc6e3e11121028f0b89b1b43b7ffdca09
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-10-30 11:22:02 +0100

    switching AsyncComm to run_until_complete, better stopping mechanism

--- async_communication.py
+++ async_communication.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import zmq
-import threading
 import asyncio
-import zmq.asyncio
 import logging
+import threading
+
+import zmq
+import zmq.asyncio
+
 import _pickle as pickle
 
 # Get the local logger
@@ -12,21 +14,22 @@ logger = logging.getLogger(__name__)
 
 
 class AsyncCommunication(threading.Thread):
-    def __init__(self, identity=None):
+    def __init__(self, local_address, callback=None, identity=None):
 
         self._target_address = None  # type:str
         self._receive_callback = None
         self._log = logging.getLogger(__name__)
 
         self.identity = identity
-
+        self.callback = callback
+        self.local_address = local_address
         self.context = None
         self.client = None
         self.server = None
         self.poller = None
         self.timeout = 1000
         self.retries = 10
-
+        self.running = False
         self.context = zmq.asyncio.Context()
         self.poller = zmq.asyncio.Poller()
         self.loop = asyncio.new_event_loop()
@@ -34,22 +37,23 @@ class AsyncCommunication(threading.Thread):
 
         name = 'AsyncCommThread'
         if identity:
-            name = name+identity
+            name = name + identity
 
         super(AsyncCommunication, self).__init__(name=name)
 
     def run(self):
         asyncio.set_event_loop(self.loop)
-        self.loop.run_forever()
+        self.running=True
+        server_future = asyncio.ensure_future(self._run_server(callback=self.callback, local_address=self.local_address), loop=self.loop)
+        try:
+            self.loop.run_until_complete(server_future)
+        except asyncio.CancelledError as e:
+            server_future.cancel()
+
 
     async def _send(self, request, remote=None):
         if self.client is None:
             print("creating client")
-            self.client = self.context.socket(zmq.DEALER)
-        else:
-            print("client already created")
-            self.poller.unregister(self.client)
-            self.client.close()
             self.client = self.context.socket(zmq.DEALER)
 
         if self.identity is not None:
@@ -77,11 +81,29 @@ class AsyncCommunication(threading.Thread):
         print('sending {} to {}'.format(request, socket_address))
         await self.client.send_multipart([pickle.dumps(request)])
 
-    async def _run_server(self, callback=None):
+        self.poller.unregister(self.client)
+        self.client.close()
+
+
+    async def _run_server(self, local_address, callback=None):
+        if not (local_address):
+            msg = 'At least TCP address must be used.'
+            raise ValueError(msg)
+        if self.identity is None:
+            self.identity = local_address.encode()
+        print('Server listening on address tcp://{}.'.format(local_address))
+
+        self.server = self.context.socket(zmq.ROUTER)
+        self.server.bind('tcp://{}'.format(local_address))
         self.poller.register(self.server, zmq.POLLIN)
         print('running server')
-        while True:
-            items = dict(await self.poller.poll(self.timeout))
+        while self.running:
+            try:
+                items = await self.poller.poll(self.timeout)
+            except (asyncio.CancelledError) as e:
+                break
+            items = dict(items)
+
             if self.server in items and items[self.server] == zmq.POLLIN:
                 print("receiving at server")
                 ident, msg = await self.server.recv_multipart()
@@ -97,21 +119,12 @@ class AsyncCommunication(threading.Thread):
                 #TODO Should this be further handled?
                 msg = pickle.loads(msg)
                 ident = pickle.loads(ident)
-                self.executor.submit(callback, data=msg, src=ident)
+                callback(data=msg, src=ident)
 
-    def run_server(self, callback=None, local_address=None):
-        if not (local_address):
-            msg = 'At least TCP address must be used.'
-            raise ValueError(msg)
-        if self.identity is None:
-            self.identity = local_address.encode()
-        print('Server listening on address tcp://{}.'.format(local_address))
-
-        self.server = self.context.socket(zmq.ROUTER)
-        self.server.bind('tcp://{}'.format(local_address))
-
-        asyncio.run_coroutine_threadsafe(
-            self._run_server(callback=callback), self.loop)
+        print("Done running server")
+        self.poller.unregister(self.server)
+        self.server.close()
+        self.stop()
 
     def send(self, request, remote=None):
         print("send {} to {}".format(request, remote))
@@ -119,8 +132,13 @@ class AsyncCommunication(threading.Thread):
             self._send(request, remote=remote), self.loop)
 
     def stop(self):
-        self.poller.unregister(self.client)
-        self.poller.unregister(self.server)
-        self.loop.stop()
-        self.context.destroy()
-
+        print("Stopping AsyncCommThread")
+        if self.running:
+            self.running = False
+        else:
+            for task in asyncio.Task.all_tasks(self.loop):
+                task.cancel()
+            self.loop.stop()
+            self.loop.close()
+            print("Stopped AsyncCommThread")
+            #self.context.destroy()
