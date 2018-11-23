078694cfff2256d2baf8329e94b073f7245cb02b
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-11-23 13:31:48 +0100

    Properly create eid for p.ptype='stop' in EventId

--- defs.py
+++ defs.py
@@ -1,14 +1,63 @@
 from collections import namedtuple
+import msgpack
+import hashlib
 
-class Allocation(namedtuple('Allocation', ['id', 'value', 'duration'])):
-    def __new__(cls, id=0, value=0, duration=0):
-        return super(Allocation, cls).__new__(cls, id, value, duration)
+packet_types = [
+    'allocation', 
+    'curr_allocation', 
+    'join', 
+    'join_ack', 
+    'allocation_ack', 
+    'stop', 
+    'stop_ack', 
+    'leave', 
+    'leave_ack'
+    ]
+
+class Allocation(namedtuple('Allocation', ['aid', 'value', 'duration'])):
+    def __new__(cls, aid=0, value=0, duration=0):
+        return super(Allocation, cls).__new__(cls, aid, value, duration)
 
 class Packet(namedtuple('Packet', ['ptype', 'payload', 'src'])):
     def __new__(cls, ptype, payload=None, src=None):
-        packet_types = ['allocation', 'curr_allocation', 'join', 'join_ack', 'allocation_ack', 'stop', 'stop_ack', 'leave', 'leave_ack']
         if ptype not in packet_types:
             raise ValueError('Undefined packet type {}'.format(ptype))
         if ptype in ['allocation', 'curr_allocation']:
             assert isinstance(payload, Allocation), 'Packet type "allocation" needs an allocation payload not a {}'.format(type(payload))
         return super(Packet, cls).__new__(cls, ptype, payload, src)
+
+def ext_pack(x):
+    if isinstance (x, Packet):
+        return msgpack.ExtType(1, msgpack.packb([x[0], x[1], x[2]], default=ext_pack, strict_types=True))
+    elif isinstance(x, Allocation):
+        return msgpack.ExtType(2, msgpack.packb([x[0], x[1], x[2]], default=ext_pack, strict_types=True))
+    return x
+
+def ext_unpack(code, data):
+    if code == 1:
+        ptype, payload, src = msgpack.unpackb(data, ext_hook=ext_unpack, encoding='utf-8')
+        return Packet(ptype, payload, src)
+    elif code == 2:
+        aid, value, duration = msgpack.unpackb(data, ext_hook=ext_unpack, encoding='utf-8')
+        return Allocation(aid, value, duration)
+
+def EventId(p: Packet or Allocation, nid=0) -> str:
+    if isinstance(p, Allocation):
+        assert isinstance(nid, str)
+        eid = hashlib.md5('allocation {} {}'.format(p.aid,nid).encode()).hexdigest()
+        return eid
+    elif p.ptype == 'allocation' or p.ptype == 'allocation_ack':
+        assert nid == 0
+        eid = hashlib.md5('allocation {} {}'.format(p.payload.aid, p.src).encode()).hexdigest()
+        return eid
+    elif p.ptype == 'stop_ack':
+        assert nid == 0
+        eid = hashlib.md5('stop {}'.format(p.src).encode()).hexdigest()
+        return eid
+    elif p.ptype == 'stop':
+        assert isinstance(nid, str)
+        eid = hashlib.md5('stop {}'.format(nid).encode()).hexdigest()
+        return eid
+    else:
+        raise ValueError('EventId not implemented for Packet type {}'.format(p.ptype))
+        
