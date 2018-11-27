1212c0730dd1bcd9b2416b3921126bae92f9acb2
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-11-28 00:56:16 +0100

    Agent's loop runs till max float, stops with StopSimulation event renamed cancel_timer to remove_timer (makes more sens) removed Agent.process and using timeouts in Agent.schedule to execute actions in callbacks

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from sys import float_info
 import hashlib
 import simpy
 from async_communication import AsyncCommunication
@@ -20,28 +21,15 @@ class Agent():
         """
         self.logger = logging.getLogger('Agent')
 
-        self.env = simpy.rt.RealtimeEnvironment(strict=True) if env is None else env
+        self.env = simpy.rt.RealtimeEnvironment(strict=False) if env is None else env
         self.timeouts = {}
-        self.running = self.env.process(self._run())
 
     def run(self):
         self.logger.info("started agent's infinite loop")
-        if isinstance(self.env, simpy.RealtimeEnvironment):
-            self.env.sync()
         try:
-            self.env.run(until=self.running)
+            self.env.run(until=float_info.max)
         except (KeyboardInterrupt, simpy.Interrupt) as e:
-            #if not self.running.processed:
-            #    self.running.interrupt()
             self.logger.debug("{}".format(e))
-
-    def _run(self):
-        while True:
-            try:
-                yield self.env.timeout(1e-2)
-            except simpy.Interrupt:
-                self.logger.info("Agent._run interrupted")
-                break
 
     def schedule(self, action, args=None, time=0, value=None):
         """ The agent's schedule function
@@ -53,24 +41,22 @@ class Agent():
 
         """
         self.logger.debug("scheduling action {}".format(action))
-        return self.env.process(
-            self.process(action=action, args=args, time=time, value=value))
-
-    def process(self, action, args, time=0, value=None):
-        try:
-            value = yield self.env.timeout(time, value=value)
-        except simpy.Interrupt:
-            self.logger.debug("interrupted action {}".format(action))
-            return
-
-        self.logger.debug("executing action {} after {} seconds".format(action, time))
+        event = self.env.event()
+        event._ok = True
+        event._value = value
+        # event = self.env.timeout(delay=time, value=value)
+        event.callbacks.append(
+            lambda e: self.logger.debug("executing action{}".format(action)))
         if args is None:
-            action()
+            event.callbacks.append(
+                lambda e: action())
         else:
-            action(**args)
-        return value
+            event.callbacks.append(
+                lambda e: action(**args))
+        self.env.schedule(event=event, delay=time)
+        return event
 
-    def stop(self, force=False):
+    def stop(self):
         """ stop the Agent.
         Behavior left for child classes
 
@@ -83,28 +69,23 @@ class Agent():
             if not v.processed and not v.triggered:
                 v.interrupt()
         if len(self.timeouts) > 0:
-            self.logger.info("remained {} timeouts not interrupted".format(len(self.timeouts)))
-        self.running.interrupt()
-        if force:
-            raise KeyboardInterrupt
+            self.logger.warning("remained {} timeouts not interrupted".format(len(self.timeouts)))
+        stop_event = self.env.timeout(0)
+        stop_event.callbacks.append(simpy.core.StopSimulation.callback)
 
     def create_timer(self, timeout, eid, msg=''):
-        self.logger.debug("creating timer {}".format(eid))
-        event = self.env.event()
+        self.logger.debug("creating timer {} for {}s".format(eid, timeout))
+        event = self.env.timeout(timeout, value=eid)
         event.callbacks.append(
             lambda event: self.logger.info("timeout expired\n {}".format(msg)))
         event.callbacks.append(
-            lambda event: self.cancel_timer(eid))
-        event_process = self.schedule(
-            action=lambda event: event.succeed(eid),
-            args={'event': event},
-            time=timeout)
-        return event_process
+            lambda event: self.remove_timer(eid))
+        self.timeouts[eid] = event
 
-    def cancel_timer(self, eid):
+    def remove_timer(self, eid):
         self.logger.debug("canceling timer {}".format(eid))
         e = self.timeouts.pop(eid, None)
         if e is None:
-            self.logger.info("no eid %s"%eid)
-        if e is not None and not (e.processed or e.triggered):
-            e.interrupt()
+            self.logger.warning("no eid %s"%eid)
+        # if e is not None and not (e.processed or e.triggered):
+        self.schedule(e.interrupt)
