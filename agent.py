3ec6a95d01093c3ad79c4980e196d8da0deba882
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-08 16:09:30 +0100

    schedule tasks on Queue, manual env stepping

--- agent.py
+++ agent.py
@@ -6,6 +6,10 @@ from scheduler import SinsEnvironment
 from async_communication import AsyncCommunication
 from defs import Packet, Allocation, EventId
 import logging
+from time import time
+from simpy.core import Infinity
+import queue
+
 logger = logging.getLogger('Agent')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -22,18 +26,29 @@ class Agent():
         """
         self.logger = logging.getLogger('Agent')
 
-        self.env = SinsEnvironment(strict=True) if env is None else env
+        self.env = None # = SinsEnvironment(strict=True, factor=0.1) if env is None else env
+        self.tasks = queue.Queue()
         self.timeouts = {}
 
     def run(self):
+        self.env = simpy.Environment(time())
         self.logger.info("started agent's infinite loop")
         # sleep for max value of int64_t = 9223372036.854775
         # see:
         # https://github.com/python/cpython/blob/f2f4555d8287ad217a1dba7bbd93103ad4daf3a8/Include/pytime.h#L19
-        try:
-            self.env.run(until=9223372036.854775)
-        except (KeyboardInterrupt, simpy.Interrupt) as e:
-            self.logger.debug("{}".format(e))
+        # self.env.run(until=9223372036.854775)
+        while True:
+            delay = self.env.peek() - time()
+            if delay <= 0:
+                self.env.step()
+                continue
+            try:
+                func = self.tasks.get(timeout = None if delay == Infinity else delay)
+            except queue.Empty:
+                continue
+            if not func:
+                return
+            func()
 
     def schedule(self, action, args=None, time=0, value=None):
         """ The agent's schedule function
@@ -57,7 +72,7 @@ class Agent():
         else:
             event.callbacks.append(
                 lambda e: action(**args))
-        self.env.schedule(event=event, delay=time)
+        self.tasks.put(lambda: self.env.schedule(event=event, delay=time))
         return event
 
     def stop(self):
@@ -75,12 +90,13 @@ class Agent():
         if len(self.timeouts) > 0:
             self.logger.warning("remained {} timeouts not interrupted".format(len(self.timeouts)))
 
-        stop_event = self.env.event()
-        stop_event._ok = True
-        stop_event._value = None
-        stop_event.callbacks.append(lambda e: logger.info("Triggering simpy StopSimulation"))
-        stop_event.callbacks.append(simpy.core.StopSimulation.callback)
-        self.env.schedule(stop_event, simpy.core.URGENT, 0)
+        self.tasks.put(None)
+        # stop_event = self.env.event()
+        # stop_event._ok = True
+        # stop_event._value = None
+        # stop_event.callbacks.append(lambda e: logger.info("Triggering simpy StopSimulation"))
+        # stop_event.callbacks.append(simpy.core.StopSimulation.callback)
+        # self.env.schedule(stop_event, simpy.core.URGENT, 0)
 
     def create_timer(self, timeout, eid, msg=''):
         self.logger.warning("creating timer {} for {}s".format(eid, timeout))
