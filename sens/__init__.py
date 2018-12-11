c8e432b7adef17c81815e364b3365bd45c8a5df6
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-11 12:31:01 +0100

    add __version__.py and __init__.py

---
+++ sens/__init__.py
@@ -0,0 +1,5 @@
+from .agent import Agent
+from .network_load import NetworkLoad
+from .network_allocator import NetworkAllocator
+from .defs import Packet, Allocation, Event
+from .async_communication import AsyncCommunication
