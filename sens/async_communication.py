332a22165a98531dd145ec1b1fcf8b78073fc80f
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-12 15:31:16 +0100

    AsyncComm doesn't inherit from a thread anymore

--- sens/async_communication.py
+++ sens/async_communication.py
@@ -19,73 +19,59 @@ logger = logging.getLogger('AsyncCommThread')
 # ch.setFormatter(formatter)
 # logger.addHandler(ch)
 
-class AsyncCommunication(threading.Thread):
-    def __init__(self, local_address, callback=None, identity=None):
+class AsyncCommunication():
+    def __init__(self, local_address = None, callback=None, identity=None):
 
-        self._target_address = None  # type:str
         self._receive_callback = None
-        self._log = logging.getLogger(__name__)
-        self.identity = identity
-        self.callback = callback
-        self.local_address = local_address
-        self.context = None
-        self.client = None
-        self.server = None
-        self.poller = None
-        self.timeout = 1000
-        self.running = False
-        self.context = zmq.asyncio.Context()
-        self.poller = zmq.asyncio.Poller()
-        self.loop = asyncio.new_event_loop()
-        self.loop.set_debug(False)
-        self.executor = ThreadPoolExecutor(max_workers=10,
+        self._identity = identity
+        self._callback = callback
+        self._local_address = local_address
+        self._client = None
+        self._server = None
+        self._timeout = 1000
+        self._running = False
+        self._context = zmq.asyncio.Context()
+        self._poller = zmq.asyncio.Poller()
+        self._loop = asyncio.new_event_loop()
+        self._executor = ThreadPoolExecutor(max_workers=10,
                                            thread_name_prefix='executor')
-        self.loop.set_default_executor(self.executor)
-        name = 'AsyncCommThread'
-        if identity:
-            name = name + identity
-
-        threading.Thread.__init__(self, name=name)
-        #super(AsyncCommunication, self).__init__(name=name)
+        self._loop.set_default_executor(self._executor)
 
     def run(self):
-        asyncio.set_event_loop(self.loop)
+        asyncio.set_event_loop(self._loop)
         self.running = True
         server_future = asyncio.ensure_future(
-            self._run_server(callback=self.callback,
-                             local_address=self.local_address),
-            loop=self.loop)
+            self._run_server(),
+            loop=self._loop)
         try:
-            self.loop.run_until_complete(server_future)
+            self._loop.run_until_complete(server_future)
         finally:
-            self.loop.close()
+            self._loop.close()
 
     async def _send(self, request, remote=None):
-        if self.client is None:
+        if self._client is None:
             logger.debug("creating client")
-            self.client = self.context.socket(zmq.DEALER)
+            self._client = self._context.socket(zmq.DEALER)
             # No lingering after socket is closed.
             # This has proven to cause problems terminating asyncio if not 0
-            self.client.setsockopt(zmq.LINGER, 100)
+            self._client.setsockopt(zmq.LINGER, 100)
 
-        if self.identity is not None:
-            logger.info("identity is %s" % self.identity)
-            identity = msgpack.packb(self.identity, encoding='utf-8')
+        if self._identity is not None:
+            logger.info("identity is %s" % self._identity)
+            identity = msgpack.packb(self._identity, encoding='utf-8')
             try:
-                self.client.setsockopt(zmq.IDENTITY, identity)
+                self._client.setsockopt(zmq.IDENTITY, identity)
             except zmq.ZMQError as zmqerror:
                 logger.error("Error setting socket identity. {}".format(zmqerror))
 
-        if not remote:
-            msg = 'no socket_address provided'
-            raise ValueError(msg)
+        assert remote is not None, 'no socket_address provided'
 
         socket_address = 'tcp://{}'.format(remote)
 
         logger.info("Connecting to {}".format(socket_address))
         try:
-            self.client.connect(socket_address)
-            self.poller.register(self.client, zmq.POLLIN)
+            self._client.connect(socket_address)
+            self._poller.register(self._client, zmq.POLLIN)
         except zmq.ZMQError as zmqerror:
             logger.error("Error connecting client socket. {}".format(zmqerror))
             raise zmqerror
@@ -96,47 +82,44 @@ class AsyncCommunication(threading.Thread):
             logger.error("Error packing {}".format(e))
 
         logger.info('sending {} to {}'.format(request, socket_address))
-        await self.client.send_multipart([p])
-        self.poller.unregister(self.client)
-        self.client.close()
-        self.client = None
+        await self._client.send_multipart([p])
+        self._poller.unregister(self._client)
+        self._client.close()
+        self._client = None
 
-    async def _run_server(self, local_address, callback=None):
-        if not local_address:
-            msg = 'At least TCP address must be used.'
-            raise ValueError(msg)
-        if self.identity is None:
-            self.identity = local_address.encode()
-        logger.debug('Server listening on address tcp://{}.'.format(local_address))
+    async def _run_server(self):
+        assert self._local_address is not None, 'local_address not set'
+        assert self._callback is not None, 'callback is not set'
+        logger.debug('Server listening on address tcp://{}.'.format(self._local_address))
 
-        self.server = self.context.socket(zmq.ROUTER)
-        self.server.bind('tcp://{}'.format(local_address))
-        self.poller.register(self.server, zmq.POLLIN)
+        self._server = self._context.socket(zmq.ROUTER)
+        self._server.bind('tcp://{}'.format(self._local_address))
+        self._poller.register(self._server, zmq.POLLIN)
         logger.info('running server')
         try:
             while self.running:
-                items = dict(await self.poller.poll(self.timeout))
-                if self.server in items and items[self.server] == zmq.POLLIN:
+                items = dict(await self._poller.poll(self._timeout))
+                if self._server in items and items[self._server] == zmq.POLLIN:
                     logger.debug("receiving at server")
-                    ident, msg = await self.server.recv_multipart()
+                    ident, msg = await self._server.recv_multipart()
                     p = msgpack.unpackb(msg, ext_hook=ext_unpack, encoding='utf-8')
                     ident = msgpack.unpackb(ident, encoding='utf-8')
                     logger.debug('server received {} from {}'.format(p, ident))
-                    await self.loop.run_in_executor(self.executor,
-                                                    callback,
+                    await self._loop.run_in_executor(self._executor,
+                                                    self._callback,
                                                     p,
                                                     ident)
         except KeyboardInterrupt:
             logger.debug("async_communication loop interrupted")
         finally:
             logger.info("stopping server")
-            self.poller.unregister(self.server)
-            self.server.close()
+            self._poller.unregister(self._server)
+            self._server.close()
 
     def send(self, request, remote=None):
         logger.debug("send {} to {}".format(request, remote))
         asyncio.run_coroutine_threadsafe(
-            self._send(request, remote=remote), self.loop)
+            self._send(request, remote=remote), self._loop)
 
     def stop(self):
         logger.info("Stopping AsyncCommThread")
