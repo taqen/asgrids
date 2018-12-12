296fc866608e338d511ccab5e6d26e70bc10444f
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-12 15:31:51 +0100

    add SmartGridSimulation to package modules

--- sens/__init__.py
+++ sens/__init__.py
@@ -3,3 +3,4 @@ from .network_load import NetworkLoad
 from .network_allocator import NetworkAllocator
 from .defs import Packet, Allocation, EventId
 from .async_communication import AsyncCommunication
+from .deploy import SmartGridSimulation
