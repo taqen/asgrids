b65edb1b2388bce01c4f938374aa5b5f85747abc
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-14 02:33:24 +0100

    update example to deploy more nodes, step to use victor_scripts/example.py

--- smart_grid_example.py
+++ smart_grid_example.py
@@ -1,17 +1,7 @@
 from sens import SmartGridSimulation
-sim = SmartGridSimulation()
+import signal
 
-allocator = sim.create_node(ntype='allocator')
-## This will be address in the simulation network
-allocator.local = "10.222.247.1:5555"
-allocator.run()
 
-remote_node = sim.create_remote_node(
-    hostname='10.222.247.98', username='ubuntu', keyfile='~/.ssh/id_rsa.pub')
-## This will be address in the simulation network
-remote_node.local = "10.222.247.98:5000"
-remote_node.run()
-remote_node.send_join("10.222.247.1:5555")
 ## Used localy to load and prepare data
 def load_csv(file, columns=[]):
     import pandas as pd
@@ -27,7 +17,7 @@ def func(node, loads):
     from sens import Allocation
     # Scheduling allocation
     for v in loads:
-        allocation = Allocation(0, v[1], 0)
+        allocation = Allocation(0, v[1], v[2])
         node.schedule(
                 action=node.allocation_handle, 
                 args={'allocation':allocation}, 
@@ -35,3 +25,35 @@ def func(node, loads):
         node.schedule(
                 action=node.allocation_report, 
                 delay=v[0])
+
+## Create SmartGridSimulation environment
+sim = SmartGridSimulation()
+
+signal.signal(signal.SIGINT, lambda x, y: sim.stop())
+
+ipv4s=['10.10.10.1','10.10.10.98','10.10.10.110','10.10.10.94','10.10.10.36','10.10.10.45']
+
+## Create a local Agent of type NetworkAllocator
+allocator = sim.create_node(ntype='allocator')
+## This will be address in the simulation network
+allocator.local = "{}:5555".format(ipv4s[0])
+## Hit Agent's run, from here on scheduled events will be executed
+allocator.run()
+
+## Create remote agents of type NetworkLoad
+for i in range(5):
+    node = sim.create_remote_node(
+        hostname=ipv4s[i+1], username='ubuntu', keyfile='~/.ssh/id_rsa.pub')
+    ## This will be address in the simulation network
+    node.local = "{}:5000".format(ipv4s[i+1])
+    node.run()
+    node.send_join("{}:5555".format(ipv4s[0]))
+
+for i in range(5):
+    f = sim.teleport(sim.conns[i], func)
+    sim.conns[i].namespace['func'] = f
+    loads = load_csv('victor_scripts/curves.csv', ['timestamp','load_%d_p'%(i+1), 'load_%d_q'%(i+1)])
+    loads = sim.deliver(sim.conns[i], loads)
+    sim.conns[i].namespace['loads'] = loads
+    sim.conns[i].execute("func(node, loads)")
+    f(sim.nodes[i+1], loads)
