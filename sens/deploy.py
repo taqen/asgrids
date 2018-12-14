cb3232156252058579daea467891f266e4a43626
Takai Eddine Kennouche <takai.kenn@gmail.com>
2018-12-14 01:09:22 +0100

    remote nodes with rpyc execute/eval, function teleport and objects deliver

--- sens/deploy.py
+++ sens/deploy.py
@@ -1,9 +1,12 @@
 from rpyc.utils.zerodeploy import DeployedServer
 import rpyc
 from rpyc.utils.helpers import BgServingThread
+from rpyc.utils.classic import teleport_function, deliver
 from plumbum import SshMachine, local
 from plumbum.path.utils import copy
 import sens
+from sens import NetworkAllocator, NetworkLoad
+import typing
 
 class SmartGridSimulation:
     def __init__(self):
@@ -13,42 +16,82 @@ class SmartGridSimulation:
         self.remote_servers = []
         self.server_threads = []
 
-    def check_remote(self, remote_server, ntype='load'):
+        # provide rpyc's deliver as a local function
+        # This function allows deliver objects to remote machines
+        self.deliver = deliver
+
+        # provide rpyc's deliver as a local function
+        # This function allows delivering objects to remote machines
+        # NOTE Expect remote node object name to be always "node"
+        # callable arguments should be rpyc's netrefs to use remote objects,
+        # and avoid re-uploading.
+        self.set_node_function = teleport_function
+
+    """
+    Make sure 'sens' library is available remotely
+    """
+    def check_remote(self, remote_server, ntype='load', python_pkg_path="~/.local/lib/python3.6/site-packages"):
         conn = remote_server.classic_connect()
         if "sens" not in conn.modules:
             rpyc.classic.upload_package(
                 conn,
                 sens,
-                "/home/ubuntu/.local/lib/python3.6/site-packages/")
+                python_pkg_path)
             return False
         else:
             return True
 
-    def create_node(self, hostname, username, keyfile, ntype='load', config={}):
+    """
+    Create node with type 'ntype' on the remote machine `hostname`
+    Returns a rpyc object wrapper, that enables handling the remote object
+    as if it was created locally.
+    """
+    def create_remote_node(self, hostname, username, keyfile, ntype='load', config={}):
         remote_machine = SshMachine(host=hostname, user=username, keyfile=keyfile)
         remote_server = DeployedServer(remote_machine)
+        # if `sens` wasn't available remotely, now we installed it
+        # but we need to reconnect to get an updated conn.modules
+        # TODO Might not be needed if using execute/eval
         if not self.check_remote(remote_server):
             remote_server.close()
             remote_server = DeployedServer(remote_machine)
 
         self.remote_machines.append(remote_machine)
         self.remote_servers.append(remote_server)
-        remote_tmp = remote_server.proc.argv[8]
-        remote_tmp = remote_machine.path(remote_tmp).up()
         conn = remote_server.classic_connect()
         serving_thread = BgServingThread(conn)
         self.server_threads.append(serving_thread)
         self.conns.append(conn)
         node = None
+
+        ## Using execute/eval allows working on a remote single namespace
+        ## useful when teleporting functions that need using remote object names
+        ## as using conn.modules create a locate but not a remote namespace member
         if ntype is 'load':
-            node = conn.modules.network_load.NetworkLoad(local="{}:5000".format(hostname))
+            conn.execute("from sens import NetworkLoad")
+            conn.execute("node=NetworkLoad()")
+            node = conn.namespace['node']
         elif ntype is 'allocator':
-            node = conn.modules.network_allocator.NetworkAllocator(local="{}:5555".format(hostname))
+            conn.execute("from sens import NetworkAllocator")
+            conn.execute("node=NetworkAllocator()")
+            node = conn.namespace['node']
         else:
             raise ValueError("Can't handle ntype == {}".format(ntype))
         self.nodes.append(node)
         return node
-    
+
+    def create_node(self, ntype='load'):
+        if ntype is 'load':
+            node = NetworkLoad()
+            self.nodes.append(node)
+            return node
+        elif ntype is 'allocator':
+            node = NetworkAllocator()
+            self.nodes.append(node)
+            return node
+    """
+    Runs the created remote objects
+    """
     def run(self):
         for node in self.nodes:
             node.run()
@@ -56,6 +99,9 @@ class SmartGridSimulation:
     def get_node(self, ind):
         return self.nodes[ind]
 
+    """
+    Stops the remotely created nodes
+    """
     def stop(self):
         for node in self.nodes:
             node.stop()
