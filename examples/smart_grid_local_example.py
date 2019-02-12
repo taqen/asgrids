c2e5883f676baa2226f886ea9eaef9de0935397a
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-02-12 12:56:26 +0100

    Upgrade smart_grid_local_example.py to new API changes

--- examples/smart_grid_local_example.py
+++ examples/smart_grid_local_example.py
@@ -18,26 +18,22 @@ def func(node, loads):
     # Scheduling allocation
     for v in loads:
         allocation = Allocation(0, v[1], v[2])
-        node.schedule(action=node.allocation_handle, 
+        node.schedule(action=node.handle_allocation, 
             args={'allocation':allocation}, 
-            delay=v[0], 
-            callbacks=[node.allocation_report])
+            delay=v[0])
 
 ## Create SmartGridSimulation environment
 sim = SmartGridSimulation()
 
 signal.signal(signal.SIGINT, lambda x, y: sim.stop())
 
-## Create a local Agent of type NetworkAllocator
-allocator = sim.create_node(ntype='allocator')
-## This will be address in the simulation network
-allocator.local = "127.0.0.1:5555"
+## Create a local Agent of type NetworkAllocator, addr will be in the simulated network
+allocator = sim.create_node(ntype='allocator', addr="127.0.0.1:5555")
 ## Hit Agent's run, from here on scheduled events will be executed
 allocator.run()
 
 ## Create remote agents of type NetworkLoad
-node = sim.create_node(ntype='load')
-node.local="127.0.0.1:5000"
+node = sim.create_node(ntype='load', addr="127.0.0.1:5000")
 node.run()
 node.schedule(node.send_join, {'dst':'127.0.0.1:5555'})
 
