040a4da3334d70c209ad5822d4ec5956d4cf4400
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-02-14 18:56:19 +0100

    handled curr_allocatiob containg as a list, moved live_plot to deploy.py

--- sens/defs.py
+++ sens/defs.py
@@ -35,9 +35,13 @@ class Packet(namedtuple('Packet', ['ptype', 'payload', 'src', 'dst'])):
     def __new__(cls, ptype, payload=None, src=None, dst=None):
         if ptype not in packet_types:
             raise ValueError('Undefined packet type {}'.format(ptype))
-        if ptype in ['allocation', 'curr_allocation']:
+        if ptype is 'allocation':
             assert isinstance(payload, Allocation), 'Packet type "allocation" needs an allocation payload not a ' \
                                                     '{}'.format(type(payload))
+        if ptype in ['curr_allocation', 'join']:
+            assert isinstance(
+                payload, list), 'Packet type "curr_allocation" needs a list containing current allocation and' \
+                ' current measure {}'.format(type(payload))
         return super(Packet, cls).__new__(cls, ptype, payload, src, dst)
 
 
@@ -51,22 +55,26 @@ def ext_pack(x):
 
 def ext_unpack(code, data):
     if code == 1:
-        ptype, payload, src, dst = msgpack.unpackb(data, ext_hook=ext_unpack, encoding='utf-8')
+        ptype, payload, src, dst = msgpack.unpackb(
+            data, ext_hook=ext_unpack, encoding='utf-8')
         return Packet(ptype, payload, src, dst)
     elif code == 2:
-        aid, p_value, q_value, duration = msgpack.unpackb(data, ext_hook=ext_unpack, encoding='utf-8')
+        aid, p_value, q_value, duration = msgpack.unpackb(
+            data, ext_hook=ext_unpack, encoding='utf-8')
         return Allocation(aid, p_value, q_value, duration)
 
 
 def EventId(p, nid=0) -> str:
     if isinstance(p, Allocation):
         assert isinstance(nid, str)
-        eid = hashlib.md5('allocation {} {}'.format(p.aid, nid).encode()).hexdigest()
+        eid = hashlib.md5('allocation {} {}'.format(
+            p.aid, nid).encode()).hexdigest()
         return eid
     elif isinstance(p, Packet):
         if p.ptype == 'allocation' or p.ptype == 'allocation_ack':
             assert nid == 0
-            eid = hashlib.md5('allocation {} {}'.format(p.payload.aid, p.src).encode()).hexdigest()
+            eid = hashlib.md5('allocation {} {}'.format(
+                p.payload.aid, p.src).encode()).hexdigest()
             return eid
         elif p.ptype == 'stop_ack':
             assert nid == 0
@@ -77,7 +85,8 @@ def EventId(p, nid=0) -> str:
             eid = hashlib.md5('stop {}'.format(nid).encode()).hexdigest()
             return eid
         else:
-            raise ValueError('EventId not implemented for Packet type {}'.format(p.ptype))
+            raise ValueError(
+                'EventId not implemented for Packet type {}'.format(p.ptype))
     else:
         raise ValueError('EventId not implemented for {}'.format(p))
 
@@ -104,129 +113,3 @@ class MeasuresProvider(object):
 
     def get_measure(self):
         return self.current_measure.get()
-
-def live_plot(buses, plot_values):
-    fig = plt.figure()
-    ax = {}
-    lines = {}
-    min_lines = {}
-    max_lines = {}
-    from math import ceil, sqrt
-    grid_dim = ceil(sqrt(len(buses)))
-    i = 1
-    for bus in buses:
-        ax[bus] = plt.subplot(grid_dim, grid_dim, i)
-        lines[bus] = ax[bus].plot([], [])[0]
-        min_lines[bus] = ax[bus].plot([], [], color='red')[0]
-        max_lines[bus] = ax[bus].plot([], [], color='red')[0]
-        i = i + 1
-    plt.subplots_adjust(hspace=0.7, bottom=0.2)
-
-    def init():
-        try:
-            for bus, a in ax.items():
-                min_value = 0.95
-                max_value = 1.05
-                a.set_title('voltage value (p.u.) - bus {}'.format(bus))
-                a.set_ylim([min_value*0.99, max_value*1.01])
-
-                lines[bus].set_data([], [])
-                min_lines[bus].set_data([0], [min_value])
-                max_lines[bus].set_data([0], [max_value])
-        except Exception as e:
-            print("Error at live_plot init {}".format(e))
-
-        artists = [line for _, line in lines.items()]
-        artists = artists + [line for _, line in min_lines.items()]
-        artists = artists + [line for _, line in max_lines.items()]
-        artists = artists + [a for _, ax in ax.items()]
-
-        return artists
-
-    def data_gen():
-        while True:
-            timestamp = {}
-            value = {}
-            try:
-                qsize = plot_values.qsize()
-                for _ in range(qsize):
-                    t, b, v = plot_values.get()
-                    if b not in buses:
-                        continue
-                    else:
-                        if b not in value:
-                            timestamp[b] = []
-                            value[b] = []
-                        timestamp[b].append(t)
-                        value[b].append(v)
-            except Exception as e:
-                print(e)
-                raise e
-            if timestamp == 0:
-                break
-            yield timestamp, value
-
-    def animate(data):
-        t, v = data
-        artists = []
-        try:
-            for bus_id in v:
-                xmin, xmax = ax[bus_id].get_xlim()
-                ymin, ymax = ax[bus_id].get_ylim()
-                if len(lines[bus_id].get_ydata()) == 0:
-                    ax[bus_id].set_xlim(max(t[bus_id]), 2 * max(t[bus_id]))
-                    # ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, max(v[bus_id]) + 0.005)
-                    ax[bus_id].relim()
-                if max(t[bus_id]) >= xmax:
-                    ax[bus_id].set_xlim(xmin, max(t[bus_id]) + 1)
-                    ax[bus_id].relim()
-                if max(v[bus_id]) >= ymax:
-                    ax[bus_id].set_ylim(ymin, max(v[bus_id]) + 0.005)
-                    ax[bus_id].relim()
-                # elif min(v[bus_id]) > ymin + 0.05:
-                #     ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, ymax)
-                #     ax[bus_id].relim()
-                if min(v[bus_id]) <= ymin:
-                    ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, ymax)
-                    ax[bus_id].relim()
-
-                xdata = np.append(lines[bus_id].get_xdata(), t[bus_id])
-                ydata = np.append(lines[bus_id].get_ydata(), v[bus_id])
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
-                lines[bus_id].set_data(xdata, ydata)
-
-                xdata = min_lines[bus_id].get_xdata()
-                ydata = min_lines[bus_id].get_ydata()
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
-                    ax[bus_id].set_xlim(min(xdata), xmax)
-
-                min_lines[bus_id].set_data(np.append(xdata, t[bus_id]), np.append(
-                    ydata, [ydata[0]]*len(t[bus_id])))
-
-                xdata = max_lines[bus_id].get_xdata()
-                ydata = max_lines[bus_id].get_ydata()
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
-                max_lines[bus_id].set_data(np.append(xdata, t[bus_id]), np.append(
-                    ydata, [ydata[0]]*len(t[bus_id])))
-
-            artists = artists + [line for _, line in lines.items()]
-            artists = artists + [line for _, line in max_lines.items()]
-            artists = artists + [line for _, line in min_lines.items()]
-            artists = artists + [a for _, a in ax.items()]
-        except Exception as e:
-            print("Exception when filling lines {}".format(e))
-        return artists
-
-    anim = animation.FuncAnimation(fig, animate, data_gen, init_func=init,
-                                   interval=10, blit=False, repeat=False)
-    try:
-        plt.autoscale(True)
-        plt.show()
-    except Exception as e:
-        print("Error at plt.show {}".format(e))
