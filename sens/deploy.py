8014d99d1521837715367cffd8821d2ca3211186
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-04-26 22:16:55 -0400

    properly interrupt controllers when simulation stops

--- sens/deploy.py
+++ sens/deploy.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -20,6 +19,18 @@ from .network_allocator import NetworkAllocator
 from .network_load import NetworkLoad
 from .defs import Allocation
 from .controller import PIController
+import logging
+
+# logging.basicConfig(filename='simulation.log',
+#                             filemode='a',
+#                             format='%(message)s',
+#                             # datefmt='%H:%M:%S',
+#                             level=logging.DEBUG)
+logger = logging.getLogger('SmartGridSimulation')
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler('simulation.log')
+fh.setLevel(logging.INFO)
+logger.addHandler(fh)
 
 
 class SmartGridSimulation(object):
@@ -52,7 +63,8 @@ class SmartGridSimulation(object):
         import os
         import sens
 
-        rpyc.classic.upload_package(conn, sens, os.path.join(python_pkg_path, "sens"))
+        rpyc.classic.upload_package(
+            conn, sens, os.path.join(python_pkg_path, "sens"))
         return False
 
     """
@@ -64,7 +76,8 @@ class SmartGridSimulation(object):
     def create_remote_node(self, hostname, username, keyfile, ntype, addr, config=None):
         if config is None:
             config = {}
-        remote_machine = SshMachine(host=hostname, user=username, keyfile=keyfile)
+        remote_machine = SshMachine(
+            host=hostname, user=username, keyfile=keyfile)
         remote_server = DeployedServer(remote_machine)
         # if `sens` wasn't available remotely, now we installed it
         # but we need to reconnect to get an updated conn.modules
@@ -103,10 +116,12 @@ class SmartGridSimulation(object):
     Creates a local node
     """
 
-    def create_node(self, ntype, addr):
+    def create_node(self, ntype, addr, identity=None):
         if ntype is 'load':
             node = NetworkLoad()
             node.local = addr
+            if identity:
+                node.identity = identity
             self.nodes[addr] = node
             return node
         elif ntype is 'allocator':
@@ -142,7 +157,7 @@ class SmartGridSimulation(object):
         self.shutdown = True
 
 
-def runpp(net, allocations_queue: Queue, measure_queues: dict, plot_queue: Queue, with_plot=False, initial_time=0):
+def runpp(net, allocations_queue: Queue, measure_queues: dict, plot_queue: Queue, with_plot=False, initial_time=0, log=False):
     """Perform power flow analysis to collect voltage values of all the buses
     
     Args:
@@ -165,6 +180,10 @@ def runpp(net, allocations_queue: Queue, measure_queues: dict, plot_queue: Queue
                     return
                 net.load.loc[net.load['name'] == name, 'p_kw'] = p_kw
                 net.load.loc[net.load['name'] == name, 'q_kw'] = q_kw
+                if log:
+                    logger.info('{}\tload_{}\t{}\t{}'.format(
+                        timestamp, name, p_kw, q_kw))
+
         except Exception as e:
             print(e)
         converged = True
@@ -200,10 +219,32 @@ def runpp(net, allocations_queue: Queue, measure_queues: dict, plot_queue: Queue
                     print(e)
                     raise(e)
 
+            if log:
+                for row in net.res_bus.iterrows():
+                    if (net.load.loc[net.load['bus'] == row[0]]['controllable'] == True).any():
+                        logger.info('{}\tbus_{}\t{}\t{}'.format(
+                            time(), row[0], row[1][0].item(), 0))
+
 
 def optimize_network_opf(net, allocator, voltage_values=None, duty_cycle=10):
     print("Optimizing network in realtime with OPF")
     while True:
+        qsize = voltage_values.qsize()  # Getting all measurements from the queue at once
+        optimize = False
+        try:
+            for _ in range(qsize):
+                nid, v = voltage_values.get()
+                if nid == v == 0:
+                    print("Terminating optimize_network_opf")
+                    return
+                if v >= 1.05:
+                    optimize = True
+            if not optimize:
+                sleep(duty_cycle)
+                continue
+        except Exception as e:
+            print(e)
+
         pp.runopp(net, verbose=False)
         try:
             c_loads = net.load[net.load['controllable'] == True]
@@ -223,24 +264,28 @@ def optimize_network_opf(net, allocator, voltage_values=None, duty_cycle=10):
                 allocator.send_allocation(nid=name, allocation=allocation)
             except Exception as e:
                 print("Error scheduing allocation: {}".format(e))
+                print("Terminating OPF controller")
+                break
         sleep(duty_cycle)
+
 
 def optimize_network_pi(net, allocator, voltage_values: Queue, duty_cycle=10):
     print("Optimizing network in realtime with PI")
     controller = PIController(maximum_voltage=400*1.05, duration=duty_cycle)
     try:
         # Listing all controllable loads, these are the PV generators that will be optimized
-        pv_gens = net.load.loc[net.load['controllable'] == True]['name'].tolist()
+        pv_gens = net.load.loc[net.load['controllable']
+                               == True]['name'].tolist()
     except Exception as e:
         print(e)
 
     while True:
         nids = []
-        gen_vs: list = [] # List of generators(PV) current voltages
-        load_vs: list = [] # List of non-generators current voltages
-        load_max_as: list = [] # List of maximum allocations allowed for non-generators
+        gen_vs: list = []  # List of generators(PV) current voltages
+        load_vs: list = []  # List of non-generators current voltages
+        load_max_as: list = []  # List of maximum allocations allowed for non-generators
         # while len(net.load['name'].tolist()) < len(gen_vs) + len(load_vs):
-        qsize = voltage_values.qsize() # Getting all measurements from the queue at once
+        qsize = voltage_values.qsize()  # Getting all measurements from the queue at once
         values: dict = {}
         # Some measures could be updates for the same nodes
         # We only take the most recent measures
@@ -248,20 +293,23 @@ def optimize_network_pi(net, allocator, voltage_values: Queue, duty_cycle=10):
         try:
             for _ in range(qsize):
                 nid, v = voltage_values.get()
+                if nid == v == 0:
+                    print("Terminating optimize_network_pi")
+                    return
                 values[nid] = v
         except Exception as e:
             print(e)
-        
-        if len(values)>0:
+
+        if len(values) > 0:
             print("Optimizing {} nodes".format(len(values)))
         for nid, v in values.items():
-            if v >=1.05:
+            if v >= 1.05:
                 print("Node {} above threshold".format(nid))
             try:
                 # We wanna know the actual voltage of the bus
-                bus_id = net.load.loc[net.load['name']==nid, 'bus'].item()
+                bus_id = net.load.loc[net.load['name'] == nid, 'bus'].item()
                 #bus_vn = net.bus['vn_kv'][bus_id].item()*1e3
-                bus_vn = 400#v
+                bus_vn = 400  # v
             except Exception as e:
                 print("Error getting bus vn: {}".format(e))
             if nid in pv_gens:
@@ -274,13 +322,15 @@ def optimize_network_pi(net, allocator, voltage_values: Queue, duty_cycle=10):
                 try:
                     # Using loads (non-generators) allocations from net as their maximum allocations (shouldn't have big effect)
                     # Maximum allocation for generators are -30kW
-                    load_max_as.append(net.load.loc[net.load['name'] == nid, 'p_kw'].item()*1e3)
+                    load_max_as.append(
+                        net.load.loc[net.load['name'] == nid, 'p_kw'].item()*1e3)
                 except Exception as e:
                     print(e)
         try:
             if len(gen_vs) > 0:
                 # print("Optimizing {}".format([nid for nid in nids]))
-                _, pv_a = controller.generate_allocations(load_vs, gen_vs, load_max_as, [-30e3]*len(gen_vs))
+                _, pv_a = controller.generate_allocations(
+                    load_vs, gen_vs, load_max_as, [-30e3]*len(gen_vs))
                 # print(len(pv_a))
             else:
                 continue
@@ -290,16 +340,19 @@ def optimize_network_pi(net, allocator, voltage_values: Queue, duty_cycle=10):
         # print("optimizing {}".format(nid))
         try:
             for a, nid in zip(pv_a, nids):
-                allocation = Allocation(a.aid, a.p_value/1e3, a.q_value/1e3, a.duration)
+                allocation = Allocation(
+                    a.aid, a.p_value/1e3, a.q_value/1e3, duty_cycle)
                 allocator.send_allocation(nid=nid, allocation=allocation)
         except Exception as e:
             print(e)
+            print("Terminating PI controller")
+            break
         values = {}
         if duty_cycle > 0:
             sleep(duty_cycle)
 
 
-def live_plot(buses, plot_values, interval=10):
+def live_plot_voltage(buses, plot_values, interval=10):
     from math import ceil, sqrt
     from matplotlib import pyplot as plt, animation
     import numpy as np
@@ -376,7 +429,6 @@ def live_plot(buses, plot_values, interval=10):
                 ymin, ymax = ax[bus_id].get_ylim()
                 if len(lines[bus_id].get_ydata()) == 0:
                     ax[bus_id].set_xlim(max(t[bus_id]), 2 * max(t[bus_id]))
-                    # ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, max(v[bus_id]) + 0.005)
                     ax[bus_id].relim()
                 if max(t[bus_id]) >= xmax:
                     ax[bus_id].set_xlim(xmin, max(t[bus_id]) + 1)
@@ -384,37 +436,144 @@ def live_plot(buses, plot_values, interval=10):
                 if max(v[bus_id]) >= ymax:
                     ax[bus_id].set_ylim(ymin, max(v[bus_id]) + 0.005)
                     ax[bus_id].relim()
-                # elif min(v[bus_id]) > ymin + 0.05:
-                #     ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, ymax)
-                #     ax[bus_id].relim()
                 if min(v[bus_id]) <= ymin:
                     ax[bus_id].set_ylim(min(v[bus_id]) - 0.005, ymax)
                     ax[bus_id].relim()
 
                 xdata = np.append(lines[bus_id].get_xdata(), t[bus_id])
                 ydata = np.append(lines[bus_id].get_ydata(), v[bus_id])
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
                 lines[bus_id].set_data(xdata, ydata)
 
                 xdata = min_lines[bus_id].get_xdata()
                 ydata = min_lines[bus_id].get_ydata()
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
-                    ax[bus_id].set_xlim(min(xdata), xmax)
 
                 min_lines[bus_id].set_data(np.append(xdata, t[bus_id]), np.append(
                     ydata, [ydata[0]]*len(t[bus_id])))
 
                 xdata = max_lines[bus_id].get_xdata()
                 ydata = max_lines[bus_id].get_ydata()
-                if len(xdata) > 200:
-                    xdata = xdata[100:]
-                    ydata = ydata[100:]
                 max_lines[bus_id].set_data(np.append(xdata, t[bus_id]), np.append(
                     ydata, [ydata[0]]*len(t[bus_id])))
+
+            artists = artists + [line for _, line in lines.items()]
+            artists = artists + [line for _, line in max_lines.items()]
+            artists = artists + [line for _, line in min_lines.items()]
+            artists = artists + [a for _, a in ax.items()]
+        except Exception as e:
+            print("Exception when filling lines {}".format(e))
+        return artists
+
+    anim = animation.FuncAnimation(fig, animate, data_gen, init_func=init,
+                                   interval=interval, blit=False, repeat=False)
+    try:
+        plt.autoscale(True)
+        plt.show()
+    except Exception as e:
+        print("Error at plt.show {}".format(e))
+
+
+def live_plot_load(loads, plot_values, interval=10):
+    from math import ceil, sqrt
+    from matplotlib import pyplot as plt, animation
+    import numpy as np
+
+    fig = plt.figure()
+    ax = {}
+    lines = {}
+    min_lines = {}
+    max_lines = {}
+    grid_dim = ceil(sqrt(len(loads)))
+    i = 1
+    for load in loads:
+        ax[load] = plt.subplot(grid_dim, grid_dim, i)
+        lines[load] = ax[load].plot([], [])[0]
+        min_lines[load] = ax[load].plot([], [], color='red')[0]
+        max_lines[load] = ax[load].plot([], [], color='red')[0]
+        i = i + 1
+    plt.subplots_adjust(hspace=0.7, bottom=0.2)
+
+    def init():
+        try:
+            for bus, a in ax.items():
+                min_value = 0.95
+                max_value = 1.05
+                a.set_title('load value (kW) - load {}'.format(bus))
+                a.set_ylim([min_value*0.99, max_value*1.01])
+
+                lines[bus].set_data([], [])
+                min_lines[bus].set_data([0], [min_value])
+                max_lines[bus].set_data([0], [max_value])
+        except Exception as e:
+            print("Error at live_plot init {}".format(e))
+
+        artists = [line for _, line in lines.items()]
+        artists = artists + [line for _, line in min_lines.items()]
+        artists = artists + [line for _, line in max_lines.items()]
+        artists = artists + [a for _, ax in ax.items()]
+
+        return artists
+
+    def data_gen():
+        while True:
+            timestamp = {}
+            value = {}
+            try:
+                qsize = plot_values.qsize()
+                if qsize == 0:
+                    yield None, None
+                for _ in range(qsize):
+                    t, b, v = plot_values.get_nowait()
+                    if b not in buses:
+                        continue
+                    else:
+                        if b not in value:
+                            timestamp[b] = []
+                            value[b] = []
+                        timestamp[b].append(t)
+                        value[b].append(v)
+            except Exception as e:
+                print(e)
+                raise e
+            if timestamp == 0:
+                break
+            yield timestamp, value
+
+    def animate(data):
+        t, v = data
+        artists = []
+        if t is None:
+            return artists
+        try:
+            for lid in v:
+                xmin, xmax = ax[lid].get_xlim()
+                ymin, ymax = ax[lid].get_ylim()
+                if len(lines[lid].get_ydata()) == 0:
+                    ax[lid].set_xlim(max(t[lid]), 2 * max(t[lid]))
+                    ax[lid].relim()
+                if max(t[bus_id]) >= xmax:
+                    ax[lid].set_xlim(xmin, max(t[lid]) + 1)
+                    ax[lid].relim()
+                if max(v[lid]) >= ymax:
+                    ax[lid].set_ylim(ymin, max(v[lid]) + 0.005)
+                    ax[lid].relim()
+                if min(v[lid]) <= ymin:
+                    ax[lid].set_ylim(min(v[lid]) - 0.005, ymax)
+                    ax[lid].relim()
+
+                xdata = np.append(lines[lid].get_xdata(), t[lid])
+                ydata = np.append(lines[lid].get_ydata(), v[lid])
+                lines[lid].set_data(xdata, ydata)
+
+                xdata = min_lines[lid].get_xdata()
+                ydata = min_lines[lid].get_ydata()
+
+                min_lines[lid].set_data(np.append(xdata, t[lid]), np.append(
+                    ydata, [ydata[0]]*len(t[lid])))
+
+                xdata = max_lines[lid].get_xdata()
+                ydata = max_lines[lid].get_ydata()
+                max_lines[lid].set_data(np.append(xdata, t[lid]), np.append(
+                    ydata, [ydata[0]]*len(t[lid])))
 
             artists = artists + [line for _, line in lines.items()]
             artists = artists + [line for _, line in max_lines.items()]
