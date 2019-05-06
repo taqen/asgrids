1ea4a8bdaf1c0d0ee8eddc5f65af0d24cea20ff0
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-05-06 16:41:25 +0200

    add some script parameters

--- examples/plot_bars.py
+++ examples/plot_bars.py
@@ -1,78 +1,125 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+import os
+parser = argparse.ArgumentParser(
+    description='Realtime Time multi-agent simulation of CIGRE LV network')
+parser.add_argument('--pi', action='store_true',
+                    help='plot PI results')
+parser.add_argument('--opf', action='store_true',
+                    help='plot OPF results')
+parser.add_argument('--output', type=str,
+                    help='output file',
+                    default='sim_bars.png')
+parser.add_argument('--high-limit', type=float,
+                    help='Total simulation time',
+                    default=1.05)
+parser.add_argument('--low-limit', type=float,
+                    help='Total simulation time',
+                    default=0.95)
+parser.add_argument('--cycles', type=str,
+                    help='',
+                    default='1,2,3,4,5,6,7,8,9,10')    
+parser.add_argument('--results', type=str,
+                    help='',
+                    default='./experiments/3')
+parser.add_argument('--runs', type=int,
+                    help='',
+                    default=5)                                      
 
-low = 0.96
-high = 1.04
+args = parser.parse_args()
+
+with_pi= args.pi
+with_opf= args.opf
+output = args.output
+low = args.low_limit
+high = args.high_limit
+cycles = [int(i) for i in args.cycles.split(',')]
+results = args.results
+runs = args.runs
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-width=0.2
-hits: list = []
-for i in range(7):
-    data = pd.read_csv('experiments/3/sim_pv_{}.log'.format(i+1), header=None, delimiter='\t')
+width=0.4
+pv_hits: list = []
+for i in range(runs):
+    data = pd.read_csv('experiments/3/sim_pv_{}.log'.format(i), header=None, delimiter='\t')
     data[0] = data[0]-data.loc[0,0]
+    data.drop_duplicates(4, inplace=True)
     voltages = data[4]
-    hits = hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())/voltages.count()]
-hits = np.array(hits)
-pv_std = np.std(hits)
-pv_mean = np.mean(hits)
+    pv_hits = pv_hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())]
+
+pv_hits = np.array(pv_hits)
+pv_std = np.std(pv_hits)
+pv_mean = np.mean(pv_hits)
+
+data = pd.read_csv('../victor_scripts/cigre/cigre_network_lv.log', header=None)
+hits = data[data[0]>=1.05][0].count()+data[data[0]<=0.95][0].count()
 
 # hits: list = []
-for i in range(10): ## Cycle
-    pv_bar = ax.bar([i+1-width], [pv_mean],
-                width=width,
-                yerr=[pv_std],
-                facecolor='red',
-                align='center',
-                alpha=0.5,
-                ecolor='black',
-                capsize=2)
-
+for i in cycles: ## Cycle
+    pv_bar = ax.bar([i-width], [pv_mean],
+            width=width,
+            yerr=[pv_std],
+            facecolor='red',
+            align='center',
+            alpha=0.5,
+            ecolor='black',
+            capsize=2)
+    pv_plot = ax.plot([cycles[0], cycles[-1]], [hits, hits], 'r--')
     pi_hits: list = []
     opf_hits: list = []
 
-    for j in range(10): ## Run
-        data = pd.read_csv('experiments/3/sim_opf_{}.{}.log'.format(i+1, j+1), header=None, delimiter='\t')
-        data[0] = data[0]-data.loc[0,0]
-        voltages = data[4]
-        opf_hits = opf_hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())/voltages.count()]
-        # print("OPF %d: "%i, hits)
-        data = pd.read_csv('experiments/3/sim_pi_{}.{}.log'.format(i+1, j+1), header=None, delimiter='\t')
-        data[0] = data[0]-data.loc[0,0]
-        voltages = data[4]
-        pi_hits = pi_hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())/voltages.count()]
-        # print("PI %d: "%i, hits)
+    for j in range(runs): ## Run
+        if with_opf:
+            data = pd.read_csv(os.path.join(results,'sim_opf_{}.{}.log'.format(i, j+1)), header=None, delimiter='\t')
+            data[0] = data[0]-data.loc[0,0]
+            data.drop_duplicates(4, inplace=True)
+            voltages = data[4]
+            opf_hits = opf_hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())]
+        if with_pi:
+            data = pd.read_csv(os.path.join(results,'sim_pi_{}.{}.log'.format(i, j+1)), header=None, delimiter='\t')
+            data[0] = data[0]-data.loc[0,0]
+            data.drop_duplicates(4, inplace=True)
+            voltages = data[4]
+            pi_hits = pi_hits + [(voltages[(voltages <= low)].count() + voltages[(voltages >= high)].count())]
+        if not with_opf and not with_pi:
+            break
     
-    opf_hits = np.array(opf_hits)
-    opf_std = np.std(opf_hits)
-    opf_mean = np.mean(opf_hits)
-    pi_hits = np.array(pi_hits)
-    pi_std = np.std(pi_hits)
-    pi_mean = np.mean(pi_hits)
+    if with_opf:
+        opf_hits = np.array(opf_hits)
+        opf_std = np.std(opf_hits)
+        opf_mean = np.mean(opf_hits)
+        opf_bar = ax.bar([i], [opf_mean],
+            width=width,
+            yerr=[opf_std],
+            facecolor='blue',
+            align='center',
+            alpha=0.5,
+            ecolor='black',
+            capsize=2)
 
-    opf_bar = ax.bar([i+1], [opf_mean],
-                width=width,
-                yerr=[opf_std],
-                facecolor='blue',
-                align='center',
-                alpha=0.5,
-                ecolor='black',
-                capsize=2)
-    pi_bar = ax.bar([i+1+width], [pi_mean],
-                width=width,
-                yerr=[pi_std],
-                facecolor='yellow',
-                align='center',
-                alpha=0.5,
-                ecolor='black',
-                capsize=2)
-    # plt.show()
-ax.set_ylabel('%% of voltage values beyond thresholds')
+    if with_pi:
+        pi_hits = np.array(pi_hits)
+        pi_std = np.std(pi_hits)
+        pi_mean = np.mean(pi_hits)
+        pi_bar = ax.bar([i+width], [pi_mean],
+                    width=width,
+                    yerr=[pi_std],
+                    facecolor='yellow',
+                    align='center',
+                    alpha=0.5,
+                    ecolor='black',
+                    capsize=2)
+
+ax.set_ylabel('Count of voltage values beyond thresholds')
 ax.set_xlabel('Control duty-cycle (s)')
-ax.set_xticks([i+1 for i in range(10)])
-ax.set_xticklabels(["%ds"%(i+1) for i in range(10)])
-ax.set_yticks([i for i in np.arange(0, 0.09, 0.02)])
-ax.set_yticklabels(["{}%".format(i*100) for i in np.arange(0, 0.09, 0.02)])
-ax.legend((pv_bar[0], opf_bar[0], pi_bar[0]), ('No Control', 'OPF Control', 'PI Control'))
-plt.savefig('sim_bars.png', dpi=300)
+ax.set_xticks(cycles)
+ax.set_xticklabels(["%ds"%(i) for i in cycles])
+# ax.set_yticks([i for i in np.arange(0, 0.09, 0.02)])
+# ax.set_yticklabels(["{}%".format(i*100) for i in np.arange(0, 0.09, 0.02)])
+# ax.legend((pv_bar[0], opf_bar[0], pi_bar[0]), ('No Control', 'OPF Control', 'PI Control'))
+ax.legend(tuple([pv_plot[0]]+[opf_bar[0] if with_opf else []]+[pi_bar[0] if with_pi else []]), tuple(['No Control']+['OPF Control' if with_opf else []]+ ['PI Control' if with_pi else []]))
+
+plt.savefig(output, dpi=300)
