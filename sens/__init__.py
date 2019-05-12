b2536befcd6935b4a9173c5618de64c298154348
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-05-12 11:20:39 +0200

    fixed __init__

--- sens/__init__.py
+++ sens/__init__.py
@@ -5,9 +5,9 @@ from .agent import Agent
 from .async_communication import AsyncCommunication
 from .controller import PIController
 from .defs import Allocation, EventId, Packet
-from .deploy import SmartGridSimulation, runpp, optimize_network_opf, optimize_network_pi#, live_plot_voltage
+from .deploy import SmartGridSimulation #, runpp, optimize_network_opf, optimize_network_pi#, live_plot_voltage
 from .network_allocator import NetworkAllocator
 from .network_load import NetworkLoad
 
-__all__ = ['Agent', 'AsyncCommunication', 'Allocation', 'EventId', 'Packet', 'live_plot', 'SmartGridSimulation',
-           'NetworkAllocator', 'NetworkLoad', 'PIController', 'runpp', 'optimize_network_opf', 'optimize_network_pi']
+__all__ = ['Agent', 'AsyncCommunication', 'Allocation', 'EventId', 'Packet', 'SmartGridSimulation',
+           'NetworkAllocator', 'NetworkLoad'] #,'live_plot', 'PIController', 'runpp', 'optimize_network_opf', 'optimize_network_pi']
