cd9c20cf6228d6e123c3ee1b6f2c23caef477a58
Takai Eddine Kennouche <takai.kenn@gmail.com>
2019-05-14 17:09:50 +0200

    updated loss plotting functions

--- examples/plot_loss.py
+++ examples/plot_loss.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from numpy import std, ceil, arange, sort, Inf, mean
 import argparse
 import os
+from pandas.api.types import is_string_dtype
 
 parser = argparse.ArgumentParser(
     description='Plotting ECDF')
@@ -26,6 +27,9 @@ parser.add_argument('--losses', nargs='+', type=int,
                     default=[0, 5, 10, 15, 20, 40, 50, 60])
 parser.add_argument('--max-vm', type=float,
                     default=1.05)
+parser.add_argument('--width', type=float,
+                    default=1)
+
 args = parser.parse_args()
 losses = args.losses
 runs = args.runs
@@ -49,24 +53,34 @@ else:
 
 assert len(tslice) == 2
 
-width=0.4
+def calculate_rate(data, slice_range: list = [0, Inf]):
+        if is_string_dtype(data[0]):
+            data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
+            data[0]=data[0].str.replace('VOLTAGE ', '')
+            data[0]=pd.to_numeric(data[0],errors='coerce')
+            data.reset_index(drop=True, inplace=True)
+        data[0]=data[0]-data.loc[0,0]
+        # sample every 1s
+        # data[0]=data[0].apply(ceil)
+        # data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
+        
+        data.drop(data[data[0]>287].index, inplace=True)
+        data.drop(data[data[0]==0].index, inplace=True)
+        data.reset_index(drop=True, inplace=True)
+        if slice_range != [0, Inf]:
+            data.drop(data[data[0]<tslice[0]].index, inplace=True)
+            data.reset_index(drop=True, inplace=True)
+            data.drop(data[data[0]>tslice[1]].index, inplace=True)
+            data.reset_index(drop=True, inplace=True)
+        return data[data[2]>=1.01][2].count()/data[2].count()
+
+width=args.width
+
 hits_opf: dict = {}
 hits_pi: dict = {}
 
 data = pd.read_csv(os.path.join(results, 'sim_no_control.log'), header=None, delimiter='\t')
-data[0]=data[0]-data.loc[0,0]
-# data[0]=data[0].apply(ceil)
-# data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
-data.drop(data[data[0]>287].index, inplace=True)
-data.drop(data[data[0]==0].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-print("slicing for no control")
-data.drop(data[data[0]<tslice[0]].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-data.drop(data[data[0]>tslice[1]].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-hits_pv =  [data[data[2]>=max_vm][2].count()/data[2].count()]
-
+hits_pv =  [calculate_rate(data)]
 
 for j in losses:
     hits_opf[j] = []
@@ -75,47 +89,14 @@ for j in losses:
     for i in runs:
         if with_opf:
             try:
-                data = pd.read_csv(os.path.join(results, 'sim_opf_{}loss.{}.log'.format(j,i)), header=None, delimiter='\t')
-                data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
-                data[0]=data[0].str.replace('VOLTAGE ', '')
-                data[0]=pd.to_numeric(data[0],errors='coerce')
-                data.reset_index(drop=True, inplace=True)
-                data[0]=data[0]-data.loc[0,0]
-                data[0]=data[0].apply(ceil)
-                data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
-                data.drop(data[data[0]>287].index, inplace=True)
-                data.drop(data[data[0]==0].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                print("slicing for opf {}% loss: {}".format(j, i))
-                # slicing
-                data.drop(data[data[0]<tslice[0]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data.drop(data[data[0]>tslice[1]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                hits_opf[j] = hits_opf[j] + [data[data[2]>=max_vm][2].count()/data[2].count()]
+                data = pd.read_csv(os.path.join(results, 'sim_opf_{}loss_5.{}.log'.format(j,i)), header=None, delimiter='\t')
+                hits_opf[j] = hits_opf[j] + [calculate_rate(data)]
             except Exception as e:
                 print("ERROR:", e)
         if with_pi:
             try:
-                data = pd.read_csv(os.path.join(results, 'sim_pi_{}loss.{}.log'.format(j,i)), header=None, delimiter='\t')
-                data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
-                data[0]=data[0].str.replace('VOLTAGE ', '')
-                data[0]=pd.to_numeric(data[0],errors='coerce')
-                data.reset_index(drop=True, inplace=True)
-                data[0]=data[0]-data.loc[0,0]
-                # data[0]=data[0].apply(ceil)
-                # data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
-                data.drop(data[data[0]>287].index, inplace=True)
-                data.drop(data[data[0]==0].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                # slicing
-                print("slicing for pi {}% loss: {}".format(j, i))
-                data.drop(data[data[0]<tslice[0]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data.drop(data[data[0]>tslice[1]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-
-                hits_pi[j] = hits_pi[j] + [data[data[2]>=max_vm][2].count()/data[2].count()]
+                data = pd.read_csv(os.path.join(results, 'sim_pi_{}loss_5.{}.log'.format(j,i)), header=None, delimiter='\t')
+                hits_pi[j] = hits_pi[j] + [calculate_rate(data)]
             except Exception as e:
                 print(e)
 
@@ -126,49 +107,46 @@ if plot_type is 'boxplot':
     ax_pi = None
     if not (with_pi or with_opf):
         exit()
-    elif with_pi and with_opf:
-        ax_opf = fig.add_subplot(121)
-        ax_pi = fig.add_subplot(122)
-    elif with_opf:
-        ax_opf = fig.add_subplot(111)
-    else:
-        ax_pi = fig.add_subplot(111)
-
-
+    ax = fig.add_subplot(111)
+    # elif with_pi and with_opf:
+    #     ax_opf = fig.add_subplot(121)
+    #     ax_pi = fig.add_subplot(122)
+    # elif with_opf:
+    #     ax_opf = fig.add_subplot(111)
+    # else:
+    #     ax_pi = fig.add_subplot(111)
+    pv_plot = ax.plot([0, 20], [hits_pv, hits_pv], color='red')
     if with_opf:
         box = []
-        ax = ax_opf
+        # ax = ax_opf
         for v in hits_opf.values():
             box.append(v)
-        opf_box=ax.boxplot(box, showfliers=False)
+        opf_box=ax.boxplot(box, showfliers=False, notch=True, patch_artist=True, boxprops=dict(facecolor="magenta"))
         pv_plot = ax.plot([0, 20], [hits_pv, hits_pv], color='red')
         ax.scatter(ax.get_xticks(), [mean(i) for i in hits_opf.values()])
         ax.set_xlabel('packet loss rate(%)')
         ax.set_ylabel('Voltage violation rate')
         ax.set_xticklabels(["{}%".format(j) for j in losses])
-        ax.set_yticks([i for i in arange(0, max(hits_pv)+0.02, 0.01)])
-        ax.set_yticklabels(["%0.1f%%"%(j*100) for j in arange(0, max(hits_pv)+0.02, 0.01)])
-        ax.legend([pv_plot[0]], ['No Control'], loc=1)
-        ax.title.set_text("OPF Control")
+        ax.set_yticks([i for i in arange(0, max(hits_pv)+0.02, 0.1)])
+        ax.set_yticklabels(["%0.1f%%"%(j*100) for j in arange(0, max(hits_pv)+0.02, 0.1)])
     if with_pi:
-        ax = ax_pi
+        # ax = ax_pi
         box = []
         for v in hits_pi.values():
             box.append(v)
-        pi_box=ax.boxplot(box, showfliers=False)
-        pv_plot = ax.plot([0, 20], [hits_pv, hits_pv], color='red')
+        pi_box=ax.boxplot(box, showfliers=False, notch=True, patch_artist=True, boxprops=dict(facecolor="blue"))
         ax.scatter(ax.get_xticks(), [mean(i) for i in hits_pi.values()])
         ax.set_xlabel('packet loss rate(%)')
         ax.set_ylabel('Voltage violation rate')
         ax.set_xticklabels(["{}%".format(j) for j in losses])
-        ax.set_yticks([i for i in arange(0, max(hits_pv)+0.02, 0.01)])
-        ax.set_yticklabels(["%0.1f%%"%(j*100) for j in arange(0, max(hits_pv)+0.02, 0.01)])
+        ax.set_yticks([i for i in arange(0, max(hits_pv)+0.02, 0.1)])
+        ax.set_yticklabels(["%0.1f%%"%(j*100) for j in arange(0, max(hits_pv)+0.02, 0.1)])
 
-        ax.legend([pv_plot[0]], ['No Control'], loc=1)
-        ax.title.set_text("PI Control")
+    ax.legend([pv_plot[0], pi_box["boxes"][0], opf_box["boxes"][0]], ['No Control', 'PI Control', 'OPF Control'], loc='upper right')
+
 else:
     ax = fig.add_subplot(111)
-    pv_plot = ax.plot([0, 20], [hits_pv, hits_pv], color='red')
+    pv_plot = ax.plot(losses, [hits_pv for i in losses], color='red')
     if with_opf:
         opf_bar = ax.bar([i-width/2 for i in hits_opf.keys()], [mean(i) for i in hits_opf.values()], yerr=[std(i) for i in hits_opf.values()], color='blue', width=width)
     if with_pi:
@@ -185,8 +163,10 @@ else:
         legend_labels = ["OPF Control"] + legend_labels
     if with_pi:
         legend_labels = ["PI Control"] + legend_labels
-
-    plt.legend(legend_items, legend_labels, loc="best")
+    
+    ax.set_yticks([i for i in arange(0, max(hits_pv)+0.02, 0.1)])
+    ax.set_yticklabels(["%0.1f%%"%(j*100) for j in arange(0, max(hits_pv)+0.02, 0.1)])
+    ax.legend(legend_items, legend_labels, loc="upper left")
     # ax.set_yticklabels(["%0.1f%%"%(j*100) for j in ax.get_yticks()])
     # ax.plot([i for i in hits_opf.keys()],[std(i) for i in hits_opf.values()])
     # ax.plot([i for i in hits_pi.keys()],[std(i) for i in hits_pi.values()])
--- examples/plot_loss_ecdf.py
+++ examples/plot_loss_ecdf.py
@@ -5,6 +5,8 @@ import numpy as np
 from statsmodels.distributions.empirical_distribution import ECDF
 import argparse
 import os
+from pandas.api.types import is_string_dtype
+from itertools import cycle
 parser = argparse.ArgumentParser(
     description='Plotting ECDF')
 parser.add_argument('--slice', type=str,
@@ -16,8 +18,14 @@ parser.add_argument('--with-pi', action="store_true",
                     help='plot PI data as well')
 parser.add_argument('--results', type=str, 
                     default='./raw')
+parser.add_argument('--runs', nargs='+', type=int,
+                    default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+parser.add_argument('--losses', nargs='+', type=int,
+                    default=[0, 5, 10, 15, 20, 40, 50, 60])
 
 args = parser.parse_args()
+losses = args.losses
+runs = args.runs
 results = args.results
 tslice = args.slice
 with_pi = args.with_pi
@@ -32,9 +40,31 @@ else:
 
 assert len(tslice) == 2
 
-losses = [0, 5, 10, 15, 20, 40, 50, 60]
-runs = [3, 4, 5, 6, 7]#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 width=0.4
+
+def get_voltages(data, slice: list = [0, Inf]):
+    assert len(slice) <=2
+    if is_string_dtype(data[0]):
+        data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
+        data[0]=data[0].str.replace('VOLTAGE ', '')
+        data[0]=pd.to_numeric(data[0],errors='coerce')
+        data.reset_index(drop=True, inplace=True)
+    data[0]=data[0]-data.loc[0,0]
+    # data[0]=data[0].apply(ceil)
+    # data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
+    # data.reset_index(drop=True, inplace=True)
+
+    data.drop(data[data[0]>287].index, inplace=True)
+    data.drop(data[data[0]==0].index, inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    print("slicing for opf {}% loss: {}".format(j, i))
+    # slicing
+    data.drop(data[data[0]<tslice[0]].index, inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    data.drop(data[data[0]>tslice[1]].index, inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    return data[2].tolist()
+
 data_opf: dict = {}
 data_pi: dict = {}
 for j in losses:
@@ -45,127 +75,90 @@ for j in losses:
             if with_opf:
                 print("reading for opf {}% loss: {}".format(j, i))
                 data = pd.read_csv(os.path.join(results, 'sim_opf_{}loss_5.{}.log'.format(j,i)), header=None, delimiter='\t')
-                data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
-                data[0]=data[0].str.replace('VOLTAGE ', '')
-                data[0]=pd.to_numeric(data[0],errors='coerce')
-                data.reset_index(drop=True, inplace=True)
-                data[0]=data[0]-data.loc[0,0]
-                # data[0]=data[0].apply(ceil)
-                # data.drop_duplicates(subset=[0,1], inplace=True, keep='last')
-                # data.reset_index(drop=True, inplace=True)
-
-                data.drop(data[data[0]>287].index, inplace=True)
-                data.drop(data[data[0]==0].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                print("slicing for opf {}% loss: {}".format(j, i))
-                # slicing
-                data.drop(data[data[0]<tslice[0]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data.drop(data[data[0]>tslice[1]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data_opf[j] = data_opf[j] + data[2].tolist()
+                data_opf[j] = data_opf[j] + get_voltages(data)
             if with_pi:
                 print("reading for pi {}% loss: {}".format(j, i))
                 data = pd.read_csv(os.path.join(results, 'sim_pi_{}loss_5.{}.log'.format(j,i)), header=None, delimiter='\t')
-                data.drop(data[data[0].str.contains('LOAD')].index, inplace=True)
-                data[0]=data[0].str.replace('VOLTAGE ', '')
-                data[0]=pd.to_numeric(data[0],errors='coerce')
-                data.reset_index(drop=True, inplace=True)
-                data[0]=data[0]-data.loc[0,0]
-                data.drop(data[data[0]>287].index, inplace=True)
-                data.drop(data[data[0]==0].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                # slicing
-                print("slicing for pi {}% loss: {}".format(j, i))
-                data.drop(data[data[0]<tslice[0]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data.drop(data[data[0]>tslice[1]].index, inplace=True)
-                data.reset_index(drop=True, inplace=True)
-                data_pi[j] = data_pi[j] + data[2].tolist()
+                data_pi[j] = data_pi[j] + get_voltages(data)
         except Exception as e:
             print(e)
 
 print("reading for no control")
 data = pd.read_csv(os.path.join(results, 'sim_no_control.log'), header=None, delimiter='\t')
-data[0]=data[0]-data.loc[0,0]
-data.drop(data[data[0]>287].index, inplace=True)
-data.drop(data[data[0]==0].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-print("slicing for no control")
-data.drop(data[data[0]<tslice[0]].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-data.drop(data[data[0]>tslice[1]].index, inplace=True)
-data.reset_index(drop=True, inplace=True)
-
-data_pv =  data[2].tolist()
+data_pv =  get_voltages(data)
 ecdf_pv = ECDF(data_pv)
 
+fig = plt.figure()
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+lines = ["-","--","-.",":"]
+linecycler = cycle(lines)
+bbox = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
+size=15
+i=0
 for j in losses:
-    ecdf_opf = ECDF(data_opf[j])
-    if with_pi:
-        ecdf_pi = ECDF(data_pi[j])
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
     if with_opf:
+        ecdf_opf = ECDF(data_opf[j])
         x = [i for i in arange(0.98, max(data_opf[j]), 0.001)]
         y = ecdf_opf(x)
-        plot_opf = ax.plot(x, y, color="blue")
-    if with_pi:    
+        ax1.plot(x, y, next(linecycler), label="%d%% loss"%j)
+        ax1.plot([1.01], [ecdf_opf(1.01)], marker='d', color='black')
+        ax1.plot([1.01, 1.01], [0, 1], '--', color='black')
+        ax1.annotate("OPF: %0.2f"%ecdf_opf(1.01), xy=(1.01, ecdf_opf(1.01)), 
+                    xycoords='data', xytext=(1.04, 0.8-i/10), textcoords='data', 
+                    bbox=bbox, size=size, #fontsize=8,
+                    arrowprops=dict(arrowstyle="->"))
+
+        ax1.plot([1.0], [ecdf_opf(1.0)], marker='d', color='black')
+        ax1.annotate("OPF: %0.2f"%ecdf_opf(1.0), xy=(1.0, ecdf_opf(1.0)), 
+                    xycoords='data', xytext=(1.04, 0.6-i/10), textcoords='data',  
+                    bbox=bbox, size=size, #fontsize=8,
+                    arrowprops=dict(arrowstyle="->"))
+        # ax1.title.set_text('OPF Control')
+    if with_pi:
+        ecdf_pi = ECDF(data_pi[j])
         x = [i for i in arange(0.98, max(data_pi[j]), 0.001)]
         y = ecdf_pi(x)
-        plot_pi = ax.plot(x, y, color="magenta")
-    x = [i for i in arange(0.98, max(data_pv), 0.001)]
-    y = ecdf_pv(x)
-    plot_pv = ax.plot(x, y, color="red")
-        
-    #------------------------------------------------------------------
-    if with_opf:
-        ax.plot([1.01, 1.01], sort([ecdf_opf(1.01), ecdf_pv(1.01)]), marker='d', color='black')
-    if with_pi:
-        ax.plot([1.01], [ecdf_pi(1.01)], marker='d', color='black')
-    ax.plot([1.01, 1.01], [0, 1], '--', color='black')
+        ax2.plot(x, y, next(linecycler), label="%d%% loss"%j)
+        ax2.plot([1.01], [ecdf_pi(1.01)], marker='d', color='black')
+        ax2.plot([1.01, 1.01], [0, 1], '--', color='black')
+        ax2.annotate("PI: %0.2f"%ecdf_pi(1.01), xy=(1.01, ecdf_pi(1.01)), 
+                    xycoords='data', xytext=(1.04, 0.8-i/10), textcoords='data', 
+                    bbox=bbox, size=size, #fontsize=8,
+                    arrowprops=dict(arrowstyle="->"))
 
-    bbox = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
-    if with_opf:
-        ax.text(1.015, ecdf_opf(1.01)+0.01, "OPF: %0.2f"%ecdf_opf(1.01), bbox=bbox, fontsize=8)
-    if with_pi:
-        ax.text(1.015, ecdf_pi(1.01), "PI: %0.2f"%ecdf_pi(1.01), bbox=bbox, fontsize=8)
-    ax.text(1.015, ecdf_pv(1.01)-0.01, "NC: %0.2f"%ecdf_pv(1.01), bbox=bbox, fontsize=8)
-    #------------------------------------------------------------------
-    
-    #------------------------------------------------------------------
-    if with_opf:
-        ax.plot([1.0, 1.0], sort([ecdf_opf(1.0), ecdf_pv(1.0)]), marker='d', color='black')
-    if with_pi:
-        ax.plot([1.0], sort([ecdf_pi(1.0)]), marker='d', color='black')
 
+        ax2.plot([1.0], sort([ecdf_pi(1.0)]), marker='d', color='black')
+        ax2.annotate("PI: %0.2f"%ecdf_pi(1.0), xy=(1.0, ecdf_pi(1.0)), 
+                    xycoords='data', xytext=(1.04, 0.6-i/10), textcoords='data', 
+                    bbox=bbox, size=size, #fontsize=8,
+                    arrowprops=dict(arrowstyle="->"))
+        # ax2.title.set_text('PI Control', fontsize=12)                    
+    i=i+1
+
+x = [i for i in arange(0.98, max(data_pv), 0.001)]
+y = ecdf_pv(x)
+for ax in [ax1, ax2]:
+    ax.plot(x, y, color="red", label="No Control")
+    ax.annotate("NC: %0.2f"%ecdf_pv(1.01), xy=(1.01, ecdf_pv(1.01)), 
+                xycoords='data', xytext=(0.97, 0.6), textcoords='data',
+                bbox=bbox, size=size, #fontsize=8,
+                arrowprops=dict(arrowstyle="->"))
+    ax.annotate("NC: %0.2f"%ecdf_pv(1.0), xy=(1.0, ecdf_pv(1.0)), 
+                xycoords='data', xytext=(0.97, 0.4), textcoords='data',
+                bbox=bbox, size=size, #fontsize=8,
+                arrowprops=dict(arrowstyle="->"))
+    ax.plot([1.01], [ecdf_pv(1.01)], marker='d', color='black')
+    ax.plot([1.0], [ecdf_pv(1.0)], marker='d', color='black')
     ax.plot([1.0, 1.0], [0, 1], '--', color='black')
+    ax.set_xticks([0.97, 1.01, 1.05])
+    ax.set_xticklabels(["0.97", "1.01", "1.05"])
+    ax.xaxis.set_tick_params(labelsize=15)
+    ax.yaxis.set_tick_params(labelsize=15)
+    ax.set_xlabel("vm (p.u.)", fontsize=20)
+    ax.set_ylabel("ECDF", fontsize=20)
+    ax.legend(fontsize=15, loc=1)
 
-    bbox = dict(boxstyle='round', facecolor='wheat', alpha=0.8)
-    if with_opf:
-        ax.text(1.005, ecdf_opf(1.0)+0.03, "OPF: %0.2f"%ecdf_opf(1.0), bbox=bbox, fontsize=8)
-    if with_pi:
-        ax.text(1.005, ecdf_pi(1.0), "PI: %0.2f"%ecdf_pi(1.0), bbox=bbox, fontsize=8)
-    ax.text(1.005, ecdf_pv(1.0)-0.03, "NC: %0.2f"%ecdf_pv(1.0), bbox=bbox, fontsize=8)
-    #------------------------------------------------------------------
-    xticks = ax.get_xticks()
-    xticks = np.append(xticks, 1.01)
-    ax.set_xticks(xticks)
-    ax.set_xlabel("vm (p.u.)")
-    ax.set_ylabel("ecdf")
-    legend_items = [plot_pv[0]]
-    if with_opf:
-        legend_items = [plot_opf[0]] + legend_items
-    if with_pi:
-        legend_items = [plot_pi[0]] + legend_items
-    legend_labels = ["No Control(NC)"]
-    if with_opf:
-        legend_labels = ["OPF Control"] + legend_labels
-    if with_pi:
-        legend_labels = ["PI Control"] + legend_labels
-    ax.legend(legend_items, legend_labels)
-
-    plt.tight_layout()
-    plt.savefig('ecdf_%dloss.png'%j, dpi=600)
-# plt.show()
+plt.tight_layout()
+plt.savefig('ecdf_loss.png', dpi=600)
+plt.show()
